51cda723ca4afab06f93a857f431cf4e0a1f9ab5
William Matheus WMCM. Caramão Marini <131700081@fatecpel.senacrs.com.br>
2019-06-03 11:17:42 -0300

    Validando metodos TODO

--- src/main/java/local/model/Filme.java
+++ src/main/java/local/model/Filme.java
@@ -1,5 +1,8 @@
 package local.model;
 
+import local.exception.FilmeException;
+import local.exception.FilmeSemEstoqueException;
+
 public class Filme {
 
 
@@ -21,6 +24,17 @@ public class Filme {
     }
 
     public void setNome(String nome) {
+        if (nome.length() < 2 || nome.length() > 99){
+            throw new FilmeException("O nome do cliente deve possuir entre 2 e 99 caracteres");
+        }
+        if(estoque < 0 || estoque > 99){
+            throw new FilmeException("Valor de estoque inválido");
+        }
+        if(precoLocacao < 1.0 || precoLocacao > 9.99){
+            throw new FilmeException("Valor de locação inválido");
+        }
+
+
         this.nome = nome;
     }
 
